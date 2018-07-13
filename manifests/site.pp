30f8e802127bd116af5eb6628356152b5f699e52
quinnmil <quinnmil@gmail.com>
2018-07-13 16:35:46 -0700

    once more, with feeling!

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,15 +3,16 @@ node ip-10-0-7-236 {
 		command => "cd /etc/puppet && git pull -q origin master",
 		user => root,
 		minute =>"*/5"
-		include sshd
+		
 	}
+	include sshd
 }
 
 node ip-10-0-7-8 {
-include sshd
+	include sshd
 }
 
 
 node ip-10-0-7-108 {  
-include sshd 
+	include sshd 
 }
