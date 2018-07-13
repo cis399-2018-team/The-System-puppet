c3317cdf2bb19d5d3a944c50ab77b343e03e79f0
Quinn Milionis <35316732+quinnmil@users.noreply.github.com>
2018-07-13 15:55:39 -0700

    small fix to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node ip-10-0-7-236 {
-	cron{ "puppet-update:
-		command: "cd /etc/puppet && git pull -q origin master",
+	cron{ "puppet-update":
+		command => "cd /etc/puppet && git pull -q origin master",
 		user => root,
 		minute =>"*/5"
 	}
