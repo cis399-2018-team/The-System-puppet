ac445002b402281fb8430d3dbfdafd4a07c1e1fd
Margiela Madman <39255541+oneofdemonez@users.noreply.github.com>
2018-07-13 17:01:06 -0700

    Update init.pp

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -1,27 +1,35 @@
+
 class sshd {
 	package {
-		"sshd":ensure => installed;
+		"sshd": ensure => installed;
 	}
+
 	file { "/etc/ssh/sshd_config":
-		source	=> [
+		source  => [
 			"puppet:///modules/sshd/$hostname/sshd.conf",
 			"puppet:///modules/sshd/sshd.conf",
 		],
-		mode	=> 444,
-		owner 	=> root,
-		group => root,
+		mode    => 444,
+		owner   => root,
+		group   => root,
+		# package must be installed before configuration file
 		require => Package["sshd"],
 	}
-	
+
 	service { "sshd":
-	
-	enable	=> true,
-	ensure	=> running,
-	hasstatus	=> true,
-	hasrestart	=> true,
-	require	=> [ Package["sshd"],
-			File["/etc/sshd.conf]"],
-	subscribe	=> File["/etc/sshd.conf"],
+		# automatically start at boot time
+		enable     => true,
+		# restart service if it is not running
+		ensure     => running,
+		# "service smartd status" returns useful service status info
+		hasstatus  => true,
+		# "service smartd restart" can restart service
+		hasrestart => true,
+		# package and configuration must be present for service
+		require    => [ Package["sshd"],
+			        File["/etc/sshd.conf"] ],
+		# changes to configuration cause service restart
+		subscribe  => File["/etc/sshd.conf"],
 	}
 	
 	ssh_authorized_key { "quinn_key":
