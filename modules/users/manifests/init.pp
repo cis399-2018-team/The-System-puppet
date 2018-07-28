e2f696f69f9be4d89bca9e146eee9785eaff5044
mhernan7 <39255541+margielamadman@users.noreply.github.com>
2018-07-28 16:15:03 -0700

    Add keys to users class

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -17,6 +17,15 @@ class users {
     file { '/home/mhernan':
     		ensure => directory,
 	}
+ssh_authorized_key { 'mhernan':
+		ensure => present,
+		user => 'mhernan',
+		type => 'ssh-rsa',
+		key => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDJ/h8vHqc5d7FGeSJNL9e8nNevxC6wzBqT6jcUDR/rhl8Oqu/ohV8GoKC8zR6cR1atEeeNCpCSWg7MFpDZcsKqCAEnY2YJ779HWHzeDYHmLO0Rw1MK26XRY1NIARtC1L6jN02owrfRgj3876R2qX+G3C1XVCNcE44T3UhepdOJDiriXz3KQVa2rdJ7aky9iPNLUfMSDnq+GAjWCabVU+RpHQb1ZQWC1C28nrh+s3qnyBQ7z1APtTm/zT+J97o1JHkCC52t+pOajuBOfPetScF3IZtyIz1Vfcal6GArrUHFw9I/d/pqaUliGWsbVntgojCU66DPHs8AfAQ8WMRtUrY1xh6T3gSMlnHhemkxpp3LSp5xSqEVEzFqN6SFRb1X/w5TRItFZEvczW5qkbbyvF0cv1aEyjcrhPuBOQFWP5/UDez62N27RJY+Dq7TjHS6C962r0ozNtwi5siIP22eHyY+9T87RuBZ98h0uHtpDNp0MRAX6gpJPPRS/oxIxQsTMK03O7EBaJR+1uyrBMioGFqqm5BlTSIIMjSML/Tzq82Uw5Kqa3iDKrDUtxvzkc2rSIKmiHXRfk4A1ySxpjgsjq7HC/6ZygfSFhphDTU6PdQg9nsedv/5gdQBcOxXyXjzTFlX4vxE1DF20YfF4OAd4VzhwwnVkXzn/0GQl4TdL77DIw==',
+		# home folder must be in place before ssh keys
+		require => User['mhernan'],
+		target => '~mhernan/.ssh/authorized_keys'
+	}
     ###
     user { 'quinnmil':
 		ensure => 'present',
@@ -29,19 +38,7 @@ class users {
     file { '/home/quinnmil':
     		ensure => directory,
 	}	
-}
-
-class keys {
-	ssh_authorized_key { 'mhernan':
-		ensure => present,
-		user => 'mhernan',
-		type => 'ssh-rsa',
-		key => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDJ/h8vHqc5d7FGeSJNL9e8nNevxC6wzBqT6jcUDR/rhl8Oqu/ohV8GoKC8zR6cR1atEeeNCpCSWg7MFpDZcsKqCAEnY2YJ779HWHzeDYHmLO0Rw1MK26XRY1NIARtC1L6jN02owrfRgj3876R2qX+G3C1XVCNcE44T3UhepdOJDiriXz3KQVa2rdJ7aky9iPNLUfMSDnq+GAjWCabVU+RpHQb1ZQWC1C28nrh+s3qnyBQ7z1APtTm/zT+J97o1JHkCC52t+pOajuBOfPetScF3IZtyIz1Vfcal6GArrUHFw9I/d/pqaUliGWsbVntgojCU66DPHs8AfAQ8WMRtUrY1xh6T3gSMlnHhemkxpp3LSp5xSqEVEzFqN6SFRb1X/w5TRItFZEvczW5qkbbyvF0cv1aEyjcrhPuBOQFWP5/UDez62N27RJY+Dq7TjHS6C962r0ozNtwi5siIP22eHyY+9T87RuBZ98h0uHtpDNp0MRAX6gpJPPRS/oxIxQsTMK03O7EBaJR+1uyrBMioGFqqm5BlTSIIMjSML/Tzq82Uw5Kqa3iDKrDUtxvzkc2rSIKmiHXRfk4A1ySxpjgsjq7HC/6ZygfSFhphDTU6PdQg9nsedv/5gdQBcOxXyXjzTFlX4vxE1DF20YfF4OAd4VzhwwnVkXzn/0GQl4TdL77DIw==',
-		# home folder must be in place before ssh keys
-		require => User['mhernan'],
-		target => '~mhernan/.ssh/authorized_keys'
-	}
-	ssh_authorized_key { 'quinnmil':
+ssh_authorized_key { 'quinnmil':
 		ensure => present,
 		user => 'quinnmil',
 		type => 'ssh-rsa',
@@ -49,5 +46,8 @@ class keys {
 		require => User['quinnmil'],
 		target => '~quinnmil/.ssh/authorized_keys'
 	}
+	
 }
 
+	
+	
